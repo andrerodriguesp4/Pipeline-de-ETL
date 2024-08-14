b00e8ee7f93780a19fc039a47ccc96a6eb98ee7c
andrerodriguesp4 <andrerodrigues2016p4@gmail.com>
2024-08-14 00:15:37 -0300

    At

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import filtrar_dados_funcoes
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
